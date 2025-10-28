8f21a5cafc4cd33c83fad534347a077ff8fcbac7
HOMIE方方 <79042804+HOMIEFF@users.noreply.github.com>
2025-10-28 09:42:35 +0800

    Remove tracking code from SiteFooter component
    Removed website statistics tracking code from SiteFooter.

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,22 +1,7 @@
-'use client'
-
-import { useEffect } from 'react'
-
 export function SiteFooter() {
-  // 添加网站统计代码
-  useEffect(() => {
-    var _mtj = _mtj || [];
-    (function () {
-      var mtj = document.createElement("script");
-      mtj.src = "https://node90.aizhantj.com:21233/tjjs/?k=layg2n82jf6";
-      var s = document.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(mtj, s);
-    })();
-  }, []);
-
   return (
     <footer className="text-center py-4 text-sm text-gray-500">
       <p>© 2024 编程爱好者网址导航. All rights reserved.</p>
     </footer>
   )
-}
+} 

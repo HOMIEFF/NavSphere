b4a5486e22d0bd8240f610e3db6c339227bb9f7f
HOMIE方方 <79042804+HOMIEFF@users.noreply.github.com>
2025-10-28 09:06:41 +0800

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,25 @@
 'use client'
 
+import { useEffect } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from '@/components/ui/toaster'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  // 添加网站统计代码
+  useEffect(() => {
+    var _mtj = _mtj || [];
+    (function () {
+      var mtj = document.createElement("script");
+      mtj.src = "https://node90.aizhantj.com:21233/tjjs/?k=layg2n82jf6";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(mtj, s);
+    })();
+  }, []);
+
   return (
     <SessionProvider>
       {children}
       <Toaster />
     </SessionProvider>
   )
-} 
+}
